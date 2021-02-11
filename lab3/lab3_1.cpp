4bcd562fb2bc0ca8007a2cccd4bc7519f91d309f
xofrio <kilory@yandex.ru>
2021-02-11 11:55:10 +0300

    lab3 changed, added 2 additional versions

---
+++ lab3/lab3_1.cpp
@@ -0,0 +1,71 @@
+#include <cstring>
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+
+constexpr int numberOfDescriptors { 2 },
+              writeDescriptor     { 1 },
+              readDescriptor      { 0 },
+              sleepTime           { 1 };
+bool flag1 { true },
+     flag2 { true };
+int fileDescriptors[numberOfDescriptors] { 0 };
+
+void *first_thread_job(void *flag) {
+    int buffer { 0 };
+
+    while(*(bool*)flag) {
+        ++buffer;
+        if (buffer == 10)
+            buffer = 1;
+        
+        ssize_t writeStatus { write(fileDescriptors[writeDescriptor], &buffer, sizeof(int)) };
+
+        if (writeStatus == -1)
+            printf("Couldn't write to buffer. Error: %s\n", strerror(errno));
+      
+        sleep(sleepTime);
+    }
+    pthread_exit(NULL);
+}
+
+void *second_thread_job(void *flag) {
+    int buffer { 0 };
+
+    while(*(bool*)flag) {
+        buffer = 0;
+
+        ssize_t readStatus { read(fileDescriptors[readDescriptor], &buffer, sizeof(int)) };
+
+        if (readStatus == -1)
+            printf("Got no message from buffer. Error: %s\n", strerror(errno));
+        else 
+            printf("Message: %d\n", buffer);
+        
+        sleep(sleepTime);
+    }
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t thread1,
+              thread2;
+
+    pipe(fileDescriptors);
+    
+    pthread_create(&thread1, NULL, &first_thread_job, &flag1);
+    pthread_create(&thread2, NULL, &second_thread_job, &flag2);
+    
+    getchar();
+    
+    flag1 = flag2 = false;
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    close(fileDescriptors[readDescriptor]);
+    close(fileDescriptors[writeDescriptor]);
+
+    return 0;
+}
---
+++ lab3/lab3_3.cpp
@@ -0,0 +1,74 @@
+#include <cstring>
+#include <errno.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+
+constexpr int numberOfDescriptors { 2 },
+              writeDescriptor     { 1 },
+              readDescriptor      { 0 },
+              sleepTime           { 1 };
+bool flag1 { true },
+     flag2 { true };
+int fileDescriptors[numberOfDescriptors] { 0 };
+
+void *first_thread_job(void *flag) {
+    int buffer { 0 };
+
+    while(*(bool*)flag) {
+        ++buffer;
+        if (buffer == 10)
+            buffer = 1;
+        
+        ssize_t writeStatus { write(fileDescriptors[writeDescriptor], &buffer, sizeof(int)) };
+
+        if (writeStatus == -1)
+            printf("Couldn't write to buffer. Error: %s\n", strerror(errno));
+      
+        sleep(sleepTime);
+    }
+    pthread_exit(NULL);
+}
+
+void *second_thread_job(void *flag) {
+    int buffer { 0 };
+
+    while(*(bool*)flag) {
+        buffer = 0;
+
+        ssize_t readStatus { read(fileDescriptors[readDescriptor], &buffer, sizeof(int)) };
+
+        if (readStatus == -1)
+            printf("Got no message from buffer. Error: %s\n", strerror(errno));
+        else 
+            printf("Message: %d\n", buffer);
+        
+        sleep(sleepTime);
+    }
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t thread1,
+              thread2;
+
+    pipe(fileDescriptors);
+    fcntl(fileDescriptors[0], F_SETFL, O_NONBLOCK);
+    fcntl(fileDescriptors[1], F_SETFL, O_NONBLOCK);
+    
+    pthread_create(&thread1, NULL, &first_thread_job, &flag1);
+    pthread_create(&thread2, NULL, &second_thread_job, &flag2);
+    
+    getchar();
+    
+    flag1 = flag2 = false;
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    close(fileDescriptors[readDescriptor]);
+    close(fileDescriptors[writeDescriptor]);
+
+    return 0;
+}
