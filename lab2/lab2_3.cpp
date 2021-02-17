c9d88c409976604e878d1fac65c21f17ccd25ebb
xofrio <kilory@yandex.ru>
2021-02-17 11:41:18 +0300

    removed last global variable

--- lab2/lab2_3.cpp
+++ lab2/lab2_3.cpp
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <unistd.h>
 
-pthread_mutex_t mutex;
-
 class Accessory {
     public:
         int numberOfSymbols,
@@ -12,13 +10,18 @@ class Accessory {
         char data;
         timespec time;
 
+        static pthread_mutex_t mutex;
+
+        static void initialize(){
+            Accessory::mutex = PTHREAD_MUTEX_INITIALIZER;
+        }
+
         Accessory(int numberOfSymbols, int sleepTime, bool flag, char data) {
             this->numberOfSymbols = numberOfSymbols;
             this->sleepTime = sleepTime;
             this->flag = flag;
             this->data = data;
         }
-
 };
 
 void *first_thread_job(void *information) {
@@ -30,12 +33,12 @@ void *first_thread_job(void *information) {
         clock_gettime(CLOCK_REALTIME, &((Accessory*)information)->time);
         *(&((Accessory*)information)->time.tv_sec) += 1;
 
-        if(!pthread_mutex_timedlock(&mutex, &((Accessory*)information)->time)) {
+        if(!pthread_mutex_timedlock(&Accessory::mutex, &((Accessory*)information)->time)) {
             for (int i { 0 }; i < numberOfSymbols; ++i) {
                 printf("%c", symbol);
                 fflush(stdout);
             }
-            pthread_mutex_unlock(&mutex);
+            pthread_mutex_unlock(&Accessory::mutex);
             sleep(sleepTime);
         }
     }
@@ -51,25 +54,25 @@ void *second_thread_job(void *information) {
         clock_gettime(CLOCK_REALTIME, &((Accessory*)information)->time);
         *(&((Accessory*)information)->time.tv_sec) += 1;
 
-        if(!pthread_mutex_timedlock(&mutex, &((Accessory*)information)->time)) {
+        if(!pthread_mutex_timedlock(&Accessory::mutex, &((Accessory*)information)->time)) {
             for (int i { 0 }; i < numberOfSymbols; ++i) {
                 printf("%c", symbol);
                 fflush(stdout);
             }
-            pthread_mutex_unlock(&mutex);
+            pthread_mutex_unlock(&Accessory::mutex);
             sleep(sleepTime);
         }
     }
     return nullptr;
 }
 
+pthread_mutex_t Accessory::mutex;
+
 int main() {
     pthread_t thread1, 
               thread2;
     Accessory forFirstThread  { 5, 1, true, '1' },
               forSecondThread { 5, 1, true, '2' };
-              
-    pthread_mutex_init(&mutex, nullptr);
 
     if (pthread_create(&thread1, nullptr, &first_thread_job, (void*)&forFirstThread))
         perror("Failed to create thread 1.");
@@ -84,8 +87,6 @@ int main() {
         perror("Failed to join thread 1.");
     if (pthread_join(thread2, nullptr))
         perror("Failed to join thread 2.");
-
-    pthread_mutex_destroy(&mutex);
 
     return 0;
 }
